2de2c16156f820d61ec9e6af2c918c60987643d4
Byron Morales <bmandrey71@gmail.com>
2024-11-10 14:53:15 -0600

    ProyectoBase
    Proyecto base con mvc que no contiene nada.

---
+++ EclipseTatuaje/EclipseTatuaje/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EclipseTatuaje.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult FormularioArtistas()
+        {
+            return View();
+        }
+
+
+        public ActionResult FormularioUsuarios()
+        {
+            return View();
+        }
+    }
+}
